e2b20c555b91f08eb3610f30b2170bff2133c0db
Decave <decave27@gmail.com>
2021-03-04 13:20:23 +0900

    fix: add source type tuple

--- revar/__init__.py
+++ revar/__init__.py
@@ -5,19 +5,19 @@ __version__ = "0.1"
 __author__ = "decave27"
 
 def replace(
-    source: Union[list, set, str], var: Dict[str, Union[str, int, bool, float]]
+    source: Union[list, set, str, tuple], var: Dict[str, Union[str, int, bool, float]]
 ):
     """
     Replace specific values ​​with custom variables.
 
     Parameters
     ==========
-    source: typing.Union[list, set, str]
+    source: typing.Union[list, set, str, tuple]
         string or list before being replaced.
     var: typing.Dict[str, typing.Union[str, int, bool, float]]
         This is the variable setting for conversion.
     """
-    if isinstance(source, (list, set)):
+    if isinstance(source, (tuple, list, set)):
         for oldstr, newstr in var.items():
             if oldstr in old:
                 source = [newstr if i == oldstr else i for i in source]
@@ -25,3 +25,5 @@ def replace(
         for oldstr, newstr in var.items():
             source = source.replace(oldstr, newstr)
     return source
+
+
